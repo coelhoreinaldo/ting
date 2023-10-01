78ccd81511cf4cc172a3e47f3cb663eef43427ef
coelhoreinaldo <scoelhor7@gmail.com>
2023-10-01 18:11:00 -0300

    ✨ feat: req 3, file_process

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -4,12 +4,11 @@ import sys
 def txt_importer(path_file):
     if not path_file.endswith(".txt"):
         print("Formato inválido", file=sys.stderr)
+        return None
     try:
         with open(path_file, "r") as file:
             file_content = file.read().split("\n")
             return file_content
     except FileNotFoundError:
         print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
-
-
-print(txt_importer("/statics/arquivo_teste.txt"))
+        return None
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,26 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
+
+def process(path_file, instance: Queue):
+    curr_file = txt_importer(path_file)
+    processed = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(curr_file),
+        "linhas_do_arquivo": curr_file,
+    }
+
+    already_processed = False
+    for process in instance._data:
+        if process["nome_do_arquivo"] == processed["nome_do_arquivo"]:
+            already_processed = True
+            break
+
+    if already_processed:
+        return None
+
+    instance.enqueue(processed)
+    print(processed)
 
 
 def remove(instance):
@@ -8,3 +29,21 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+
+
+instance = Queue()
+
+
+# if __name__ == "__main__":
+#     print(
+#         process(
+#             "../statics/arquivo_teste.txt",
+#             instance,
+#         )
+#     )
+#     print(
+#         process(
+#             "../statics/arquivo_teste.txt",
+#             instance,
+#         )
+#     )
