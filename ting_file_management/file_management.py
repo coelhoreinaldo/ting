8fecbb15f29e7356733a5741f77adb417139856e
coelhoreinaldo <scoelhor7@gmail.com>
2023-10-01 16:26:29 -0300

    ✨ feat: req 2, create file_management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file, "r") as file:
+            file_content = file.read().split("\n")
+            return file_content
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
