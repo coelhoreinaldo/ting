5aa82012c6f2591725e9fd8b612c88d21a6001ee
coelhoreinaldo <scoelhor7@gmail.com>
2023-10-02 11:04:24 -0300

    🧪 test: priority queue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,25 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+from pytest import raises
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority_queue = PriorityQueue()
+    priority_queue.enqueue({"qtd_linhas": 10})
+    priority_queue.enqueue({"qtd_linhas": 2})
+    priority_queue.enqueue({"qtd_linhas": 29})
+    priority_queue.enqueue({"qtd_linhas": 4})
+    priority_queue.enqueue({"qtd_linhas": 40})
+    assert priority_queue.__len__() == 5
+    assert priority_queue.high_priority.__len__() == 2
+    assert priority_queue.regular_priority.__len__() == 3
+
+    priority_queue.dequeue()
+    assert priority_queue.__len__() == 4
+    assert priority_queue.high_priority.__len__() == 1
+    assert priority_queue.regular_priority.__len__() == 3
+
+    found_elem = priority_queue.search(0)
+    assert found_elem == {"qtd_linhas": 4}
+
+    with raises(IndexError, match="Índice Inválido ou Inexistente"):
+        priority_queue.search(99)
